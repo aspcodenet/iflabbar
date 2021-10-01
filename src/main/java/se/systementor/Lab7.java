e39c5474ef463964b34b3fb73944d4c37ac28a14
Stefan Holmberg <stefan.holmberg@systementor.se>
2021-10-01 15:23:07 +0200

    lab7 fix

--- src/main/java/se/systementor/Lab7.java
+++ src/main/java/se/systementor/Lab7.java
@@ -2,16 +2,25 @@ package se.systementor;
 
 public class Lab7 {
     public void execute(){
-        System.out.println("Enter height:");
-        int height = Integer.parseInt(System.console().readLine());
-        System.out.println("Enter width:");
-        int width = Integer.parseInt(System.console().readLine());
+        
+        System.out.println("Enter money:");
+        float money = Float.parseFloat(System.console().readLine());
+        System.out.println("Har du rabatt (J)?");
+        
+        boolean rabatt = false;
+        if(System.console().readLine().charAt(0) == 'J')
+            rabatt = true;
 
-        for(int row = 0; row < height; row++){
-            for(int col = 0; col < height; col++){
-                System.out.print("X");
-            }
-            System.out.println("");
+        if(money > 15 && money < 25 && rabatt == false)
+            System.out.println("Du kan köpa en liten hamburgare.");
+        else if(money > 15 && money < 25 && rabatt)
+            System.out.println("Du kan köpa en liten hamburgare och en pommes frites.");
+        else if(money > 25 && money <= 50 && rabatt == false)
+            System.out.println("Du kan köpa en stor hamburgare.");
+        else if(money > 25 && money <= 50 && rabatt)
+            System.out.println("Du kan köpa en stor hamburgare och en pommes frites.");
+        else if( money > 60 || ( money >= 50 && money <= 60 && rabatt ) )            
+            System.out.println("Du kan köpa ett meal med dryck");
         }
 
 
